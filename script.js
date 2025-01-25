2e84a2a813946a5b6bcf96a98cc89eb3c59e5f15
daniel yu <fruit.broz@gmail.com>
2025-01-25 21:18:47 +0100

    declared all variables in js

---
+++ script.js
@@ -0,0 +1,18 @@
+const arrayBtn = document.getElementById('array-btn');
+const stackBtn = document.getElementById('stack-btn');
+const queueBtn = document.getElementById('stack-btn');
+const linkedListBtn = document.getElementById('linked-list-btn');
+const binaryTreeBtn = document.getElementById('binary-tree-btn');
+
+const arrayAddBtn = document.getElementById('array-add-btn');
+const arrayRemoveBtn = document.getElementById('array-remove-btn');
+const pushBtn = document.getElementById('push-btn');
+const popBtn = document.getElementById('pop-btn');
+const enqueueBtn = document.getElementById('enqueue-btn');
+const dequeueBtn = document.getElementById('dequeue-btn');
+const addNodeListBtn = document.getElementById('add-node-list-btn');
+const removeNodeBtn = document.getElementById('remove-node-btn');
+const addNodeTreeBtn = document.getElementById('add-node-tree-btn');
+const traverseTreeBtn = document.getElementById('traverse-tree-btn');
+
+const homeBtn = document.getElementById('home-btn');
